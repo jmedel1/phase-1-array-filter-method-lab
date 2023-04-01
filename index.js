36c90bbd0b9bcfcb859fe05427b2de61053e844d
jael <jael.medel30@gmail.com>
2023-03-31 23:21:11 -0500

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+  }
+
+const result = findMatching(drivers, "bobby");
+console.log(result);
+
+function fuzzyMatch (drivers, string) {
+const stringLength = string.length;
+return drivers.filter(driver => driver.slice(0, stringLength) === string);
+}
+const match = fuzzyMatch(driver, "Sa");
+console.log(match);
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+} 
